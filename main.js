a91de29d6c85b4e46805046de2c40d568a8b19ff
Anurag Kumar <47923173+anuragabcr@users.noreply.github.com>
2024-07-08 15:06:44 +0530

    completed

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 const fs = require('fs');
-
+const http=require('http')
 // TODO: Write the message "Hello, World!" to the file "output.txt"
 
+
+  fs.writeFile("./output.txt","Hello, World!",(err)=>{
+    if(err){
+        console.error('Error writing to file:', err);
+        throw err; 
+    }
+    console.log('Message has been written to output.txt');
+  })
